bce006bc0ca5288afcac9d7d5cb3ae14f6c02de7
Adrian Price-Whelan <583379+adrn@users.noreply.github.com>
2026-01-24 09:26:09 -0500

    Add optimize_iterative to export names

--- src/pollux/models/__init__.py
+++ src/pollux/models/__init__.py
@@ -1,5 +1,6 @@
 from . import transforms
 from .cannon import Cannon
+from .iterative import optimize_iterative
 from .lux import Lux, LuxModel
 from .transforms import *
 
@@ -7,5 +8,6 @@ __all__ = [  # noqa: PLE0604
     "Cannon",
     "Lux",
     "LuxModel",  # TODO: deprecated
+    "optimize_iterative",
     *transforms.__all__,
 ]
